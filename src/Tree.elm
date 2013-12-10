12064b88355b6fcb2891a566d594dc9aa0dd1a0f
Eliot Alter <Eliot.Alter@gmail.com>
2013-12-10 02:25:25 -0500

    Added tree helper functions to use path indexes

--- src/Tree.elm
+++ src/Tree.elm
@@ -8,8 +8,45 @@ leaf a = Tree a []
 treeMap : (a -> b) -> Tree a -> Tree b
 treeMap f (Tree x y) = Tree (f x) (map (treeMap f) y)
 
-treeZipWidth : (a -> b -> c) -> Tree a -> Tree b -> Tree c
-treeZipWidth f (Tree x1 y1) (Tree x2 y2) = Tree (f x1 x2) (zipWith (treeZipWidth f) y1 y2)
+treeZipWith : (a -> b -> c) -> Tree a -> Tree b -> Tree c
+treeZipWith f (Tree x1 y1) (Tree x2 y2) = Tree (f x1 x2) (zipWith (treeZipWith f) y1 y2)
+
+{- Creates a tree with the paths to each node -}
+treeGetPaths : Tree a -> Tree ([Int])
+treeGetPaths tree =
+    let mapIndexes : Int -> [a] -> [(a, Int)]
+        mapIndexes i list =
+            case list of
+                []      -> []
+                x :: xs -> (x, i) :: mapIndexes (i + 1) xs
+
+        applyTuple : (a -> b -> c) -> (a, b) -> c
+        applyTuple f (a, b) = f a b
+
+        treeGetPaths' : Tree a -> [Int] -> Tree ([Int])
+        treeGetPaths' (Tree x xs) prefix = 
+            let addPrefix : (a, Int) -> (a, [Int])
+                addPrefix (a, i) = (a, i :: prefix)
+
+                --TODO: add type annotation. Elm refuses the correct annotation?
+                paths = map addPrefix <| mapIndexes 0 xs
+            in Tree prefix <| map (applyTuple treeGetPaths') paths
+    in treeMap reverse <| treeGetPaths' tree []
+
+treeAtPath : Tree a -> [Int] -> Maybe a
+treeAtPath tree path =
+    let maybeHead : [a] -> Maybe a
+        maybeHead xs = case xs of
+                            []     -> Nothing
+                            x :: _ -> Just x
+
+        nth : Int -> [a] -> Maybe a
+        nth i list = maybeHead <| drop i list
+    in case path of
+        []        -> Just (treeData tree)
+        (x :: xs) -> (case nth x (treeSubtree tree) of
+                           Nothing   -> Nothing
+                           Just node -> treeAtPath node xs)
 
 treeData : Tree a -> a
 treeData (Tree d _) = d
---
+++ src/menus/indexes.elm
@@ -0,0 +1,25 @@
+import open Tree
+import open Menu
+
+menuSpec : [Tree String]
+menuSpec = [Tree "Main" [leaf "About",
+                         leaf "Updates"],
+            Tree "File" [leaf "New",
+                         Tree "Foo" [leaf "Text",
+                                     Tree "Phone" [leaf "bar",
+                                                   leaf "baz"],
+                                    leaf "Open"]]]
+
+onlyJust : Maybe a -> a
+onlyJust a = case a of
+                  Just b -> b
+
+stringsAgain : Tree String -> Tree String
+stringsAgain tree = treeMap (onlyJust . treeAtPath tree) (treeGetPaths tree)
+
+indexes : [Tree String]
+indexes = map (\s -> treeMap show <| treeGetPaths s) menuSpec
+
+--main = renderMenu <| map (treeMap constant) indexes
+main = renderMenu <| map (treeMap constant . stringsAgain) menuSpec
+
