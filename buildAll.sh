b4e23a853efea98ffe5cb8c351182e5601a68771
Eliot Alter <Eliot.Alter@gmail.com>
2014-01-05 21:13:32 -0500

    Made the build script

---
+++ buildAll.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd src
+elm -m *.elm menus/*.elm
+cd build
+
+fixElmRuntime() {
+    file=$1
+    temp=$(mktemp)
+    regex='s/src=\".*elm-runtime.js\"/src=\"\/elm-runtime.js\"/g'
+    sed "$regex" "$file" > "$temp"
+    mv "$temp" "$file"
+}
+
+export -f fixElmRuntime
+
+find . -name '*.html' -exec bash -c 'fixElmRuntime $@' _ {} \;
+
